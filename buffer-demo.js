d2c992b2e5de6348a103b0503ed45774937b8316
SAISURYA <saisurya015@gmail.com>
2025-06-02 14:50:31 +0530

    buffer

---
+++ buffer-demo.js
@@ -0,0 +1,26 @@
+// buffer -> those are queryObjects. handle binary data
+// file system operations, cryptography, image processing
+
+const buffOne = Buffer.alloc(10) //allocate a buffer of 10 bytes -> zeros
+console.log(buffOne);
+
+
+const buffFromString = Buffer.from("Hello");
+console.log(buffFromString);
+
+
+const buffFromArrayOfIntergers = Buffer.from([1, 2, 3, 4, 5, 0])
+console.log(buffFromArrayOfIntergers);
+
+buffOne.write('sai');
+console.log('after writing node.js to bufferOne', buffOne.toString());
+
+console.log(buffFromString[0]);
+
+console.log(buffFromString.slice(0, 3));
+
+const concatBuffs = Buffer.concat([buffOne, buffFromString]);
+console.log(concatBuffs);
+
+console.log(concatBuffs.toJSON());
+
