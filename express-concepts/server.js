c27c3d11dc06cbad42c3ba02cf0a1c7ab7d2594b
SAISURYA <saisurya015@gmail.com>
2025-06-03 13:10:29 +0530

    api/url versionaning

--- express-concepts/server.js
+++ express-concepts/server.js
@@ -1,8 +1,12 @@
+// -> major releases -> API -> v1, v2
+
+
 require('dotenv').config()
 const express = require('express')
 const { configureCors } = require('./config/corsConfig');
 const { requestLogger, addTimeStamp } = require('./middleware/customMiddleware');
-const { globalErrorhandler} = require('./middleware/errorHandler')
+const { globalErrorhandler} = require('./middleware/errorHandler');
+const {urlVersioning} = require('./middleware/apiVersioning')
 
 
 const app = express();
@@ -15,6 +19,8 @@ app.use(addTimeStamp)
 
 app.use(configureCors());
 app.use(express.json());
+
+app.use('api/v1', urlVersioning('v1'))
 
 app.use(globalErrorhandler)
 
